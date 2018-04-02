2192561a11cf0dc02d20c17378ce0bc413483d44
BallGame <35738815+mona217ajar@users.noreply.github.com>
2018-04-02 22:56:19 +1000

    new anim

---
+++ BallGame/Assets/_Completed-Game/Scripts/AnimationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationController : MonoBehaviour {
+
+    public Animator optionsMenu;
+    public Animator mainMenu;
+
+    public void playAnimation()
+    {
+        optionsMenu.Play("options");
+    }
+
+    public void MainMenu()
+    {
+        mainMenu.Play("menu");
+    }
+}
