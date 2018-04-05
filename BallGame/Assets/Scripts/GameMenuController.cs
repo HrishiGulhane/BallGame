25fa6b7b8c82fea92d73fffe653005e6e11fe1e0
BallGame <35738815+mona217ajar@users.noreply.github.com>
2018-04-05 12:17:34 +1000

    sabconnected

---
+++ BallGame/Assets/Scripts/GameMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuController : MonoBehaviour {
+
+	public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void QuitToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+}
+
